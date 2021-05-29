1e4578afbde4ae313bc5afcb8af398c8fde1c600
nbr41to <nbr.41to@gmail.com>
2021-05-29 13:41:15 +0900

    EmojiMartののサンプルがあります

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,21 @@ import { auth } from './config/firebase';
 import { Login, Signup, Room } from './pages'; // まとめてimportできるのはなぜか？
 import { Auth, LoggedInRoute } from './utils';
 
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+import { useState } from 'react';
+import { Emoji } from 'emoji-mart';
+
 function App() {
   // console.log(process.env.REACT_APP_HELOO); // .envの確認
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedEmoji, setSelectedEmoji] = useState('');
+
+  const emojiSelect = (emoji) => {
+    console.log(emoji);
+    setSelectedEmoji(emoji.native);
+    setIsOpen(false);
+  };
 
   return (
     <Router>
@@ -20,6 +33,10 @@ function App() {
         <li><Link to='/signup'>SIGNUP</Link></li>
       </ul>
       <button onClick={() => auth.signOut()}>logout</button>
+      <button onClick={() => setIsOpen(true)}>emoji</button>
+      {selectedEmoji}
+      <Emoji emoji={{ id: 'santa', skin: 3 }} size={16} />
+      {isOpen && <Picker set='apple' onSelect={(emoji) => emojiSelect(emoji)} />}
       <Auth>
         <Switch>
           <LoggedInRoute exact path='/' component={Room} />
