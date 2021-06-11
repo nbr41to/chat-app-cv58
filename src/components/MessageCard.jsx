1ae02e45b0f32350410c2f1410153c443718cbd4
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-06-11 23:51:31 +0900

    add emoji-mart
    MessageCardに絵文字スタンプ機能を追加しました。
    しょぼいです。

--- src/components/MessageCard.jsx
+++ src/components/MessageCard.jsx
@@ -1,6 +1,19 @@
 import dayjs from 'dayjs';
+
+import 'emoji-mart/css/emoji-mart.css';
+import { Picker } from 'emoji-mart';
+import { useState } from 'react';
+import { Emoji } from 'emoji-mart';
+
 const MessageCard = ({ message }) => {
   const { name, content, sendAt } = message;
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedEmoji, setSelectedEmoji] = useState('');
+
+  const emojiSelect = (emoji) => {
+  setSelectedEmoji(emoji.native);
+  setIsOpen(!isOpen);
+  };
 
   return (
     <div >
@@ -11,6 +24,13 @@ const MessageCard = ({ message }) => {
       <span style={{ fontStyle: 'italic' }}>
         {' '}[{dayjs(sendAt.toDate()).format('YYYY/MM/DD HH:MM:ss')}]
       </span>
+      {selectedEmoji}
+      <button onClick={() => setIsOpen(!isOpen)}>えもじ</button>
+      {isOpen ? (
+        <Picker set='apple' onSelect={(emoji) => emojiSelect(emoji)} />
+      ) : (
+      <></>
+      )}
     </div>
   );
 };
